8a20f9ef3ca98d55793c61ed4234537931bfa7da
Jayson Acosta <JaysonJAcosta5704.code@gmail.com>
2023-11-04 04:38:02 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import OpenAI from 'openai';
+import cors from 'cors';
+
+dotenv.config();
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: 'Hello from CodeX!',
+    })
+});
+
+app.post('/', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+
+        const response = await openai.completions.create({
+                model:"text-davinci-003",
+                prompt:`${prompt}`,
+                temperature: 0,
+                max_tokens: 500,
+                top_p: 1,
+                frequency_penalty: 0.5,
+                presence_penalty: 0,
+        });
+
+        console.log(response);
+        res.status(200).send({
+            bot: response.choices[0].text,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error });
+    }
+});
+
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
+
+/*
+if(notworking){
+    work();
+}
+*/
